4058cf2355ffd6487bf1a6c0476a99217e9e7561
CursoEGG001 <soygecko@gmail.com>
2023-06-02 21:04:56 -0300

    Primero lo animalito

---
+++ TeoriaEjercicios/src/Entidad/Gato.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidad;
+
+/**
+ *
+ * @author pc
+ */
+public class Gato extends Animal {
+
+    public Gato() {
+        
+    }
+
+    @Override
+    public void hacerRuido() {
+        super.hacerRuido();
+        System.out.println("Bueno! MIAU!");// Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+}
