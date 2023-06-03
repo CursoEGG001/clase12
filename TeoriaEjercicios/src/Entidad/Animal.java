25eb01a7af4eb2ed583495f3931d68fdc30de96a
CursoEGG001 <soygecko@gmail.com>
2023-06-02 21:04:56 -0300

    Primero lo animalito

---
+++ TeoriaEjercicios/src/Entidad/Animal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidad;
+
+/**
+ *
+ * @author pc
+ */
+public class Animal {
+
+    public Animal() {
+    }
+
+    public void hacerRuido() {
+        System.out.println("Haz un ruido! Animal!");
+    }
+
+}
---
+++ TeoriaEjercicios/src/clase12/TeoriaEjercicios.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase12;
+
+import Entidad.Animal;
+import Entidad.Gato;
+import Entidad.Perro;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author pc
+ */
+public class TeoriaEjercicios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        List<Animal> losAnimalitos = new ArrayList<>();
+        Animal a = new Animal();
+        Animal b = new Perro();
+        Animal c = new Gato();
+
+        losAnimalitos.add(a);
+        losAnimalitos.add(b);
+        losAnimalitos.add(c);
+        
+        for (Animal losAnimalito : losAnimalitos) {
+            losAnimalito.hacerRuido();
+        }
+    }
+
+}
