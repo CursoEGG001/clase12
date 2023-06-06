63697628c48227941fe50d9ef03ea884c6999af4
CursoEGG001 <soygecko@gmail.com>
2023-06-05 21:04:04 -0300

    Un lindo listado de clases!

---
+++ EExtras03/src/clase12/EExtra03.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase12;
+
+import Servicio.HospedajeServicio;
+
+/**
+ *
+ * @author pc
+ */
+public class EExtra03 {
+    public static void main(String[] args) {
+        HospedajeServicio.iniciar();
+    }
+    
+    
+    
+}
