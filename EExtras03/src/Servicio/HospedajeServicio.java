8104bd77b467d5a8ab2e3f100ef74396023c30d7
CursoEGG001 <soygecko@gmail.com>
2023-06-14 21:52:52 -0300

    Un menú...pero no del restaurant!

--- EExtras03/src/Servicio/HospedajeServicio.java
+++ EExtras03/src/Servicio/HospedajeServicio.java
@@ -12,17 +12,21 @@ import Entidad.Hotel5Estrellas;
 import Entidad.Residencia;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 /**
  *
  * @author pc
  */
 public class HospedajeServicio {
-    
+
     /**
      * @param args the command line arguments
      */
     public static void iniciar() {
+
+        Scanner opc = new Scanner(System.in, "UTF-8").useDelimiter("\n");
+
         // Crear los alojamientos
         Hotel5Estrellas hotel1 = new Hotel5Estrellas("Hotel A", "Dirección 1", "Localidad 1", "Gerente A", 100, 200, 10, "A", "Restaurante A", 80, 5, 10, 3);
         Hotel4Estrellas hotel2 = new Hotel4Estrellas("Hotel B", "Dirección 2", "Localidad 2", "Gerente B", 80, 150, 8, "B", "Restaurante B", 50);
@@ -34,11 +38,55 @@ public class HospedajeServicio {
         alojamientos.add(hotel2);
         alojamientos.add(camping1);
         alojamientos.add(residencia1);
+
+        int elija;
+
+        do {
+
+            System.out.println("Elija una opción: "
+                    + "\n1. Consulta de Hoteles"
+                    + "\n2. Ordenar los alojamientos de Más caro a barato"
+                    + "\n3. Mostrar los restaurantes"
+                    + "\n4. Mostrar las residencias"
+                    + "\n 5. Salir.");
+            elija = opc.nextInt();
+            switch (elija) {
+                case 1:
+                    consultaHoteles(alojamientos);
+
+                    break;
+                case 2:
+
+                    presentarDescHoteles(alojamientos);
+
+                    break;
+                case 3:
+
+                    presentarRestaurants(alojamientos);
+
+                    break;
+                case 4:
+
+                    presentarResidencias(alojamientos);
+                    break;
+                case 5:
+                    System.out.println("¡Hasta luego!");
+                    break;
+                default:
+                    System.out.println("No es una opción Válida");
+            }
+        } while (elija != 5);
+    }
+
+    private static void consultaHoteles(ArrayList<Alojamiento> alojamientos) {
         // Consulta: todos los alojamientos
         System.out.println("Todos los alojamientos:");
         for (Alojamiento alojamiento : alojamientos) {
             System.out.println(alojamiento.nombre);
         }
+    }
+
+    private static void presentarDescHoteles(ArrayList<Alojamiento> alojamientos) {
         // Consulta: todos los hoteles de más caro a más barato
         System.out.println("\nHoteles de más caro a más barato:");
         Collections.sort(alojamientos, (a1, a2) -> {
@@ -51,6 +99,9 @@ public class HospedajeServicio {
                 System.out.println(alojamiento.nombre + " - Precio habitación: $" + alojamiento.calcularPrecioHabitacion());
             }
         }
+    }
+
+    private static void presentarRestaurants(ArrayList<Alojamiento> alojamientos) {
         // Consulta: todos los campings con restaurante
         System.out.println("\nCampings con restaurante:");
         for (Alojamiento alojamiento : alojamientos) {
@@ -61,6 +112,9 @@ public class HospedajeServicio {
                 }
             }
         }
+    }
+
+    private static void presentarResidencias(ArrayList<Alojamiento> alojamientos) {
         // Consulta: todas las residencias que tienen descuento
         System.out.println("\nResidencias con descuento:");
         for (Alojamiento alojamiento : alojamientos) {
@@ -72,5 +126,5 @@ public class HospedajeServicio {
             }
         }
     }
-    
+
 }
